48f4332eb5d478d28ef404b2bfe06b839bfcd2e8
Aurora Chen <99832432+aur0rachen@users.noreply.github.com>
2022-03-22 04:02:53 +0000

    quads 1 & 2

--- Sketch.java
+++ Sketch.java
@@ -16,20 +16,41 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255, 250, 253);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    //lines 
+    line(width / 2, 0, width / 2, height);
+    line(0, height / 2, width, height / 2);
+    
+    // Quad 1
+    for (int lineX = width / 20; lineX < width / 2; lineX += width / 20){
+      line(lineX, 0, lineX, height / 2);
+    }
+    for (int lineY = height / 20; lineY < height / 2; lineY += height / 20){
+      line(0, lineY, width / 2, lineY);
+    }
+    
+    // Quad 2 
+    int circleSize = width / 20;
+    
+    for (int circleX = width / 2 + width / 20; circleX > width / 2; circleX += width / 10){
+      for (int circleY = height / 20; circleY < height / 2; circleY += height / 10){
+        ellipse(circleX, circleY, circleSize, circleSize);
+      }
+    }
+
+    // Quad 3
+    
+
+
+
+    
   }
   
   // define other methods down here.
