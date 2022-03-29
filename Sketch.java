9bd417a273d935d291f87033744504be6c471395
Aurora Chen <99832432+aur0rachen@users.noreply.github.com>
2022-03-29 21:18:08 +0000

    complete

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ import processing.core.PApplet;
 public class Sketch extends PApplet {
 	
   public void settings() {
-    size(200, 200);
+    size(400, 400);
   }
 
   public void setup() {
@@ -21,39 +21,43 @@ public class Sketch extends PApplet {
     line(0, height / 2, width, height / 2);
   
     // Quad 1
-    for (int lineX = width / 20; lineX < width / 2; lineX += width / 20){
-      line(lineX, 0, lineX, height / 2);
+    for (int intLineX = width / 20; intLineX < width / 2; intLineX += width / 20){
+      line(intLineX, 0, intLineX, height / 2);
     }
-    for (int lineY = height / 20; lineY < height / 2; lineY += height / 20){
-      line(0, lineY, width / 2, lineY);
-    }
+    for (int intLineY = height / 20; intLineY < height / 2; intLineY += height / 20){
+      line(0, intLineY, width / 2, intLineY);
+    } 
     
     // Quad 2 
-    int circleSize = width / 20;
-    
-    for (int circleX = width / 2 + width / 20; circleX > width / 2; circleX += width / 10){
-      for (int circleY = height / 20; circleY < height / 2; circleY += height / 10){
-        ellipse(circleX, circleY, circleSize, circleSize);
+    int intCircleSize = width / 20;
+    fill(255, 0, 187);
+    for (int intCircleX = width / 2 + width / 20; intCircleX > width / 2 && intCircleX <= width; intCircleX += width / 10){
+      for (int intCircleY = height / 20; intCircleY < height / 2 && intCircleY <= height; intCircleY += height / 10){
+        ellipse(intCircleX, intCircleY, intCircleSize, intCircleSize);
       }
     }
 
     // Quad 3
-    for(int i = 0; i < width / 2; i++){
-      int color = i * 255 / (width/2);
-      stroke(color);
+    for(int i = 0; i < width / 2 && i < width; i++){
+      int intColor = i * 255 / (width/2);
+      stroke(intColor);
       line(i, height / 2, i, height);
     }
     
     // Quad 4
     stroke(0);
-    translate((float)(width * 0.75), (float)(height * 0.75));
     
-    for(int petal = 0; petal < 8; petal ++){
+    translate((float)(width * 0.75), (float)(height * 0.75));
+
+    fill(255, 166, 0);
+    for(int intPetal = 0; intPetal < 8; intPetal ++){
       rotate(PI / 4);
       ellipse(width / 16, height / 80, width / 5, height / 20);
     }
-    
-    int flowerCircle = width / 12;
-    ellipse(0, 0, flowerCircle, flowerCircle);
+
+    noStroke();
+    fill(118, 194, 109);
+    int intFlowerCircle = width / 12;
+    ellipse(0, 0, intFlowerCircle, intFlowerCircle);
   }
 }
